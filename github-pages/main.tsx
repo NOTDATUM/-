562b00f55296202b279ff22e842567c1100683d8
notdatum <notdatum@gmail.com>
2026-08-21 02:14:37 +0900

    Connect GitHub Pages client to AWS server

--- github-pages/main.tsx
+++ github-pages/main.tsx
@@ -10,7 +10,7 @@ declare global {
   }
 }
 
-window.__BE_API_URL__ = import.meta.env.VITE_BE_API_URL?.trim();
+window.__BE_API_URL__ = import.meta.env.VITE_BE_API_URL?.trim() || "https://3-107-160-0.sslip.io";
 window.__BE_STATIC_MODE__ = false;
 
 createRoot(document.getElementById("root")!).render(
